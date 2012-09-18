ed71492922f108a1c8fe76741e771f075b7e1c2a
Oliver Lassen <privat.oliver.lassen@gmail.com>
2012-09-18 17:19:42 +0200

    Main updated

---
+++ src/BackboneCRM/BackboneCRM.Web.UI/Content/Scripts/main.js
@@ -0,0 +1,16 @@
+﻿/// <reference path="Vendor/Require/require-jquery.js" />
+//Configure Require
+
+require.config({
+    baseUrl: "/Content/Scripts",
+    paths: {
+        backbone: "Vendor/Backbone/backbone.min",
+        underscore: "Vendor/Backbone/underscore.min"
+    }
+});
+
+require(["underscore", "backbone"], function (_, Backbone) {
+
+    alert("Got it?" + $("body").html());
+
+});
