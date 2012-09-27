6e58dfa5b47e5e2a8704a4e7458b97bb6d20cfa7
Oliver Lassen <privat.oliver.lassen@gmail.com>
2012-09-27 16:35:16 +0200

    App module defined

---
+++ src/BackboneCRM/BackboneCRM.Web.UI/Content/Scripts/App.js
@@ -0,0 +1,13 @@
+﻿define("App", ["underscore", "backbone", "marionette"], function (_, Backbone, Marionette) {
+
+    var app = new Backbone.Marionette.Application();
+
+    app.addInitializer(function () {
+
+        alert("Hello");
+
+    });
+
+    return app;
+
+});
